5a386dae411447abc3b05b44033556cd5f69e55d
PngWnA <2016320146@korea.ac.kr>
2021-03-21 16:51:34 +0900

    [API/Auth] Added auth endpoint

---
+++ src/router/auth/auth.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express';
+import { getConnection } from 'typeorm';
+
+import { generatePassword, comparePassword } from '../../lib/auth';
+
+import Auth from '../../lib/model/auth';
+import User from '../../lib/model/user';
