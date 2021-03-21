23217cfd06b89537a88d667f0d13cf0987ecd16f
PngWnA <2016320146@korea.ac.kr>
2021-03-21 16:34:34 +0900

    [Rollback]

--- src/router/accounts/accounts.ts
+++ src/router/accounts/accounts.ts
@@ -6,72 +6,20 @@ import { generatePassword, comparePassword } from '../../lib/auth';
 import Auth from '../../lib/model/auth';
 import User from '../../lib/model/user';
 
-const err: string = 'GET /account/unregister is NOT implemented.';
 
 const register: RequestHandler = async (req, res, next) => {
-  const {
-    id, pw, sid, belong, name, email,
-  } = req.body;
-
-  console.dir(req.body);
-
-  // Check if parameters are all set.
-  const params = [id, pw, sid, belong, name, email].map((param) => Object.keys(param)[0]);
-  const invalid = params.filter((param) => param === undefined);
-  if (invalid.length) {
-    const code = '[INVALID] NULL_PARAMETER';
-    console.dir(invalid);
-    const msg = `Following param(s) is(are) null: ${id ? '' : 'id '}${pw ? '' : 'pw '}${sid ? '' : 'sid '}${belong ? '' : 'belong '}${name ? '' : 'name '}${email ? '' : 'email'}`;
-    res.status(400).json({ code, msg });
-    return next();
-  }
-  // Check if each id, sid, email is duplicated.
-  const db = getConnection();
-  const rows = await db.getRepository(User).find({
-    where: [
-      { id },
-      { sid },
-      { email },
-    ],
-  });
-
-  // Prepare Query.
-  const user = new User();
-  user.id = id;
-  user.name = name;
-  user.sid = sid;
-  user.belong = belong;
-  user.email = email;
-
-  const auth = new Auth();
-  auth.uid = user;
-  auth.digest = await generatePassword(pw);
-  auth.oauth2 = false;
-
-  // Do transaction.
-  await db.getRepository(User).save(user);
-  await db.getRepository(Auth).save(auth);
-
   return next();
 };
 
 const unregister: RequestHandler = (req, res, next) => {
-  res.send(err);
   return next();
 };
 
 const login: RequestHandler = (req, res, next) => {
-  res.send(err);
   return next();
 };
 
 const test: RequestHandler = async (req, res, next) => {
-  const db = getConnection();
-  const users = await db.getRepository(User).find();
-  const auths = await db.getRepository(Auth).find();
-
-  console.log(users);
-  console.log(auths);
   return next();
 };
 
