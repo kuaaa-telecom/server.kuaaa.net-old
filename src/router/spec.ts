00629923de98ccca7accebdea6c1c29e04d626f2
PngWnA <2016320146@korea.ac.kr>
2021-06-27 15:23:16 +0900

    [Swagger] Moved API specification to spec.ts

---
+++ src/router/spec.ts
@@ -0,0 +1,9 @@
+/**
+ * GET /
+ * @summary This is the root path of API Server 
+ * @return {object} 200 - Hello message from server if it is working.
+ * @example response - 200 - Hello message
+ * {
+ *  "msg":"Hello KUAAA.<br>By Pngwna. @ 2021"
+ * }
+ */
---
+++ src/router/users/spec.ts
@@ -0,0 +1,24 @@
+/**
+ * GET /users/
+ * @summary Returns all users existing in kuaaa.net user DB
+ * @return {object} 200 - Returns json containing all users
+ * @return {object} 401 - Returns error message for unauthorized request
+ * @return {object} 403 - Returns error message for forbidden request of given users
+ * @return {object} 500 - Returns error message for internal server error
+ * @example response - 200 - List of users
+ * {
+ *  "msg":"Not Implemented"
+ * }
+ * @example response - 401 - Unauthorized
+ * {
+ *  "msg":"Unauthorized request"
+ * }
+ * @example response - 403 - Forbidden
+ * {
+ *  "msg":"Forbidden request"
+ * }
+ * @example response - 500 - Internal Server error
+ * {
+ *  "msg":"Something went wrong..."
+ * }
+ */
