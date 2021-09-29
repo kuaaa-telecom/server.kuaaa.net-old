7fe0c473f51ddb80f2d565f5a40156a078be13ca
PngWnA <2016320146@korea.ac.kr>
2021-09-30 00:24:57 +0900

    [Hotfix] Enable custom host/port

--- src/init.ts
+++ src/init.ts
@@ -1,13 +1,14 @@
 // Entry point of KUAAA Server.
 import server from './server';
 
-const port = process.env.PORT || 31413;
+const host = process.env.BACKEND_HOST || '127.0.0.1';
+const port = Number(process.env.BACKEND_PORT) || 31413;
 
 const init = `
 Hello KUAAA. \n
-Server is listening on 0.0.0.0::${port}.
+Server is listening on ${host}::${port}.
 `;
 
 server.listen(
-  port, () => console.log(init),
+  port, host, () => console.log(init),
 );
